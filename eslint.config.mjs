9450dd8cc751734a3aedc810b0c5fd817b332ebc
Stephane Mfuni <stephuni35@gmail.com>
2025-07-03 15:21:21 +0300

    `Update .eslintignore and eslint.config.mjs to include additional ignored directories`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,9 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  {
+    ignores: ["app/generated/prisma/**/*", "node_modules/**/*", ".next/**/*"],
+  },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
